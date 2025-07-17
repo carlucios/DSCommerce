58598421bcd12a5800f32052b77f11be92c8ede1
carlucios <carlucios@gmail.com>
2025-07-17 17:55:15 -0300

    Add user entity

---
+++ src/main/java/com/devsuperior/dscommerce/Entities/User.java
@@ -0,0 +1,60 @@
+package com.devsuperior.dscommerce.Entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Campo obrigatório")
+    private String name;
+
+    @Email(message = "Email inválido")
+    @NotBlank(message = "Campo obrigatório")
+    @Column(unique = true)
+    private String email;
+
+    @Pattern(regexp = "^\\(\\d{2}\\) \\d{4,5}-\\d{4}$", message = "Formato de telefone deve ser (99) 99999-9999")
+    private String phone;
+
+    @NotBlank(message = "Campo obrigatório")
+    @Size(min = 6, message = "A senha deve ter pelo menos 6 caracteres")
+    private String password;
+
+    @NotBlank(message = "Campo obrigatório")
+    @Past(message = "Data de nascimento deve ser anterior à data atual")
+    private String birthDate;
+
+    @ManyToMany
+    @JoinTable(
+        name = "tb_user_role",
+        joinColumns = @JoinColumn(name = "user_id"),
+        inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles;
+
+    public void addRole(Role role) {
+        roles.add(role);
+    }
+
+    public void removeRole(Role role) {
+        roles.remove(role);
+    }
+
+    public boolean hasRole(String roleName) {
+        return roles.stream()
+                .anyMatch(role -> role.getAuthority().equals(roleName));
+    }
+}
