52cd8b154d3970c9421c15d96edee0247ac68c32
carlucios <carlucios@gmail.com>
2025-07-17 18:44:09 -0300

    Add BCrypt

--- src/main/java/com/devsuperior/dscommerce/config/SecurityConfig.java
+++ src/main/java/com/devsuperior/dscommerce/config/SecurityConfig.java
@@ -2,10 +2,17 @@ package com.devsuperior.dscommerce.config;
 
 import org.springframework.context.annotation.*;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }   
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
--- src/main/resources/db/migration/V2__seed_roles_and_user_roles.sql
+++ src/main/resources/db/migration/V2__seed_roles_and_user_roles.sql
@@ -2,8 +2,8 @@ INSERT INTO tb_roles (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_roles (authority) VALUES ('ROLE_ADMIN');
 
 INSERT INTO tb_users (id, name, email, phone, password, birth_date) VALUES
-(1, 'Maria Santos', 'maria@example.com', '(11) 99999-9999', '123456', '1990-01-01'),
-(2, 'Joao Silva', 'joao@example.com', '(22) 98888-8888', '123456', '1985-05-05');
+(1, 'Maria Santos', 'maria@example.com', '(11) 99999-9999', '$2a$10$JWI.Jnl3Djgq1NEgnZtgXOdZzwpH3/Z51A/2ks6IT8ExuhYumj4iC', '1990-01-01'),
+(2, 'Alex Silva', 'alex@example.com', '(22) 98888-8888', '$2a$10$JWI.Jnl3Djgq1NEgnZtgXOdZzwpH3/Z51A/2ks6IT8ExuhYumj4iC', '1985-05-05');
 
 INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
