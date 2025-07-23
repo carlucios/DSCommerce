31f93c40bf5faa66dbbfcca8cbeecd893515039a
carlucios <carlucios@gmail.com>
2025-07-23 15:31:12 -0300

    Add Role and Category repositories

--- src/main/java/com/devsuperior/dscommerce/repositories/CategoryRepository.java
+++ src/main/java/com/devsuperior/dscommerce/repositories/CategoryRepository.java
@@ -3,5 +3,8 @@ package com.devsuperior.dscommerce.repositories;
 import com.devsuperior.dscommerce.entities.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Optional<Category> findByName(String name);
 }
